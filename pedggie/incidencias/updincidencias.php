90423b8f4f43d7f66a8f1cb9625ae714c117db3b
Alex Sanmartín <isanmartin8a@gmail.com>
2017-06-02 09:07:34 +0200

    Añado modificaciones.
    Signed-off-by: Alex Sanmartín <isanmartin8a@gmail.com>

---
+++ pedggie/incidencias/updincidencias.php
@@ -0,0 +1,21 @@
+<?php 
+// Recuperamos datos del formulario
+$idIncidencia=$_POST['idIncidencia'];
+$idMaterial=$_POST['idMaterial'];
+$FechaIncidencia=$_POST['FechaIncidencia'];
+$Incidencia=$_POST['Incidencia'];
+$FechaSolucion=$_POST['FechaSolucion'];
+$Solucion=$_POST['Solucion'];
+$idUsuario=$_POST['idUsuario'];
+
+// conectamos con la BD
+include("./../conexion.php");
+// creamos consulta
+$sql="UPDATE incidencias SET idMaterial='$idMaterial', FechaIncidencia='$FechaIncidencia', Incidencia='$Incidencia', FechaSolucion='$FechaSolucion', Solucion='$Solucion', idUsuario='$idUsuario' WHERE idIncidencia='$idIncidencia'";
+
+// ejecutamos la consulta
+mysqli_query($conexion, $sql) or die("Error en la consulta de insercion $sql");
+mysqli_close($conexion);
+//
+header("location:modincidencias.php");
+?>
